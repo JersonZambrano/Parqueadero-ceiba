33e63eacdedbfcc74464f6d888ebda22553b2900
Jerson Rubiel Zambrano Angulo - Ceiba Software <jerson.zambrano@ceiba.com.co>
2018-05-11 15:29:53 -0500

    se ajusta nuevo proyecto

---
+++ parqueadero/src/main/java/com/parqueadero/parqueaderoPostgres/ParqueaderoPostgresApplication.java
@@ -0,0 +1,12 @@
+package com.parqueadero.parqueaderoPostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueaderoPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueaderoPostgresApplication.class, args);
+	}
+}
