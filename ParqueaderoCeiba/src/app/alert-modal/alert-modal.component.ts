aedb5a59d83bf2ba12b09b122718a84dea494072
Jerson Rubiel Zambrano Angulo - Ceiba Software <jerson.zambrano@ceiba.com.co>
2018-05-23 13:33:57 -0500

    se ajusta pantalla con detalles

---
+++ ParqueaderoCeiba/src/app/alert-modal/alert-modal.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit , Input} from '@angular/core';
+
+@Component({
+  selector: 'app-alert-modal',
+  templateUrl: './alert-modal.component.html',
+  styleUrls: ['./alert-modal.component.css']
+})
+export class AlertModalComponent implements OnInit {
+
+  @Input() mensaje: string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
