f08bf0f584004ff606d23b16fbffb7c94a1e9082
Jerson Rubiel Zambrano Angulo - Ceiba Software <jerson.zambrano@ceiba.com.co>
2018-05-25 11:01:20 -0500

    se ajustan pantallas y se generan pruebas funcionales

--- parqueadero/src/test/java/com/parqueadero/parqueaderoPostgresTestIT/ParqueaderoTestIT.java
+++ parqueadero/src/test/java/com/parqueadero/parqueaderoPostgresTestIT/ParqueaderoTestIT.java
@@ -22,17 +22,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.parqueadero.dao.RegistroParqueaderoRepository;
 
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ParqueaderoTestIT {
-	
+
 	@Autowired
-	RegistroParqueaderoRepository daoRegistros;	
-	
+	RegistroParqueaderoRepository daoRegistros;
+
 	public static final String MENSAJE_SACAR_VEHICULO_INEXISTENTE = "El vehículo no se encuentra registrado actualmente en el parqueadero";
-	public static final String MENSAJE_VEHICULO_YA_REGISTRADO= "El vehículo ya se encuentra en el parqueadero";
-	public static final String MENSAJE_VEHICULO_REGISTRADO_OK= "El vehículo fue registrado con éxito";
+	public static final String MENSAJE_VEHICULO_YA_REGISTRADO = "El vehículo ya se encuentra en el parqueadero";
+	public static final String MENSAJE_VEHICULO_REGISTRADO_OK = "El vehículo fue registrado con éxito";
+	private static final String CHAR_LIST = "bcdefghijklmnopqrstuvwxyzBCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+	private static final int RANDOM_STRING_LENGTH = 10;
 	private static WebDriver driver = null;
 
 	@BeforeClass
@@ -68,7 +69,7 @@ public class ParqueaderoTestIT {
 				.findElement(By.xpath("/html/body/app-root/app-registrar-parqueadero/div/app-alert-modal/div/h5"));
 		String mensaje = alertaModalMensaje.getText();
 
-		assertEquals(mensaje,MENSAJE_VEHICULO_REGISTRADO_OK);
+		assertEquals(mensaje, MENSAJE_VEHICULO_REGISTRADO_OK);
 	}
 
 	@Test
@@ -115,7 +116,7 @@ public class ParqueaderoTestIT {
 				.findElement(By.xpath("/html/body/app-root/app-registrar-parqueadero/div/app-alert-modal/div/h5"));
 		String mensaje = alertaModalMensaje.getText();
 
-		assertEquals(mensaje,MENSAJE_VEHICULO_YA_REGISTRADO);
+		assertEquals(mensaje, MENSAJE_VEHICULO_YA_REGISTRADO);
 	}
 
 	@Test
@@ -205,7 +206,7 @@ public class ParqueaderoTestIT {
 	public void registrarYconsultarVehiculoTest() {
 
 		String placa = generarStringRando();
-		
+
 		WebElement botonRegistrarNav = driver.findElement(By.xpath("/html/body/app-root/div[1]/span[1]"));
 		botonRegistrarNav.click();
 
@@ -225,95 +226,117 @@ public class ParqueaderoTestIT {
 		WebElement alertaModal = driver
 				.findElement(By.xpath("/html/body/app-root/app-registrar-parqueadero/div/app-alert-modal"));
 		wait.until(ExpectedConditions.visibilityOf(alertaModal));
-		
+
 		// busca el veh�culo
 		WebElement botonSalirNav = driver.findElement(By.xpath("/html/body/app-root/div[1]/span[3]"));
 		botonSalirNav.click();
-		
+
 		WebElement placaSalirElement = driver
 				.findElement(By.xpath("/html/body/app-root/app-consultar-vehiculos/div/div[4]/form/div[1]/div/input"));
 		placaSalirElement.sendKeys(placa);
 
 		WebElement botonConsulta = driver.findElement(By.className("btn-primary"));
 		botonConsulta.click();
-		
+
 		WebDriverWait wait2 = new WebDriverWait(driver, 2);
 		WebElement tablaElement = driver
 				.findElement(By.xpath("/html/body/app-root/app-consultar-vehiculos/div/div[4]/form/div[3]"));
 		wait2.until(ExpectedConditions.visibilityOf(tablaElement));
-		
+
 		WebElement valorPlaca = driver.findElement(By.xpath("//*[@id=\"tablaRegistros\"]/tbody/tr/td[2]"));
-		
+		System.out.println(valorPlaca.getText() + "placa en tabla--------------");
+		System.out.println(placa + "placa real--------------------");
+
 		assertEquals(valorPlaca.getText(), placa);
 	}
-	
-	
 
-//	/**
-//	 * prueba que se encarga de verificar el registrar salida para un vehiculo que
-//	 * no se encuntra registrado
-//	 */
-//	@Test
-//	public void registrarConsultarRetirarVehiculo() {
-//
-//		String placa = generarStringRando();
-//		
-//		WebElement botonRegistrarNav = driver.findElement(By.xpath("/html/body/app-root/div[1]/span[1]"));
-//		botonRegistrarNav.click();
-//
-//		WebElement placaElement = driver.findElement(
-//				By.xpath("/html/body/app-root/app-registrar-parqueadero/div/div[3]/form/div[1]/div/input"));
-//		placaElement.sendKeys(placa);
-//
-//		WebElement tipoVehiculoElement = driver.findElement(
-//				By.xpath("/html/body/app-root/app-registrar-parqueadero/div/div[3]/form/div[2]/div/select"));
-//		tipoVehiculoElement.sendKeys("CARRO");
-//
-//		WebElement botonRegistrar = driver
-//				.findElement(By.xpath("/html/body/app-root/app-registrar-parqueadero/div/div[3]/form/div[3]/button"));
-//		botonRegistrar.click();
-//
-//		WebDriverWait wait = new WebDriverWait(driver, 2);
-//		WebElement alertaModal = driver
-//				.findElement(By.xpath("/html/body/app-root/app-registrar-parqueadero/div/app-alert-modal"));
-//		wait.until(ExpectedConditions.visibilityOf(alertaModal));
-//		
-//		// busca el veh�culo
-//		WebElement botonSalirNav = driver.findElement(By.xpath("/html/body/app-root/div[1]/span[3]"));
-//		botonSalirNav.click();
-//		
-//		WebElement placaSalirElement = driver
-//				.findElement(By.xpath("/html/body/app-root/app-consultar-vehiculos/div/div[4]/form/div[1]/div/input"));
-//		placaSalirElement.sendKeys(placa);
-//
-//		WebElement botonConsulta = driver.findElement(By.className("btn-primary"));
-//		botonConsulta.click();
-//		
-//		WebDriverWait wait2 = new WebDriverWait(driver, 2);
-//		WebElement tablaElement = driver
-//				.findElement(By.xpath("/html/body/app-root/app-consultar-vehiculos/div/div[4]/form/div[3]"));
-//		wait2.until(ExpectedConditions.visibilityOf(tablaElement));
-//		
-//		
-//		
-//		
-//		WebElement valorPlaca = driver.findElement(By.xpath("//*[@id=\"tablaRegistros\"]/tbody/tr/td[2]"));
-//		
-//		assertEquals(valorPlaca.getText(), placa);
-//	}
-	
+	// /**
+	// * prueba que se encarga de verificar el registrar salida para un vehiculo que
+	// * no se encuntra registrado
+	// */
+	// @Test
+	// public void registrarConsultarRetirarVehiculo() {
+	//
+	// String placa = generarStringRando();
+	//
+	// WebElement botonRegistrarNav =
+	// driver.findElement(By.xpath("/html/body/app-root/div[1]/span[1]"));
+	// botonRegistrarNav.click();
+	//
+	// WebElement placaElement = driver.findElement(
+	// By.xpath("/html/body/app-root/app-registrar-parqueadero/div/div[3]/form/div[1]/div/input"));
+	// placaElement.sendKeys(placa);
+	//
+	// WebElement tipoVehiculoElement = driver.findElement(
+	// By.xpath("/html/body/app-root/app-registrar-parqueadero/div/div[3]/form/div[2]/div/select"));
+	// tipoVehiculoElement.sendKeys("CARRO");
+	//
+	// WebElement botonRegistrar = driver
+	// .findElement(By.xpath("/html/body/app-root/app-registrar-parqueadero/div/div[3]/form/div[3]/button"));
+	// botonRegistrar.click();
+	//
+	// WebDriverWait wait = new WebDriverWait(driver, 2);
+	// WebElement alertaModal = driver
+	// .findElement(By.xpath("/html/body/app-root/app-registrar-parqueadero/div/app-alert-modal"));
+	// wait.until(ExpectedConditions.visibilityOf(alertaModal));
+	//
+	// // busca el veh�culo
+	// WebElement botonSalirNav =
+	// driver.findElement(By.xpath("/html/body/app-root/div[1]/span[3]"));
+	// botonSalirNav.click();
+	//
+	// WebElement placaSalirElement = driver
+	// .findElement(By.xpath("/html/body/app-root/app-consultar-vehiculos/div/div[4]/form/div[1]/div/input"));
+	// placaSalirElement.sendKeys(placa);
+	//
+	// WebElement botonConsulta = driver.findElement(By.className("btn-primary"));
+	// botonConsulta.click();
+	//
+	// WebDriverWait wait2 = new WebDriverWait(driver, 2);
+	// WebElement tablaElement = driver
+	// .findElement(By.xpath("/html/body/app-root/app-consultar-vehiculos/div/div[4]/form/div[3]"));
+	// wait2.until(ExpectedConditions.visibilityOf(tablaElement));
+	//
+	//
+	//
+	//
+	// WebElement valorPlaca =
+	// driver.findElement(By.xpath("//*[@id=\"tablaRegistros\"]/tbody/tr/td[2]"));
+	//
+	// assertEquals(valorPlaca.getText(), placa);
+	// }
 
-	
 	@AfterClass
 	public static void cerrarDriver() {
 		driver.quit();
 	}
 
 	private String generarStringRando() {
-		byte[] array = new byte[7]; // length is bounded by 7
-		new Random().nextBytes(array);
-		return "C" + new String(array, Charset.forName("UTF-8"));
 
+		StringBuffer randStr = new StringBuffer();
+		for (int i = 0; i < RANDOM_STRING_LENGTH; i++) {
+			int number = getRandomNumber();
+			char ch = CHAR_LIST.charAt(number);
+			randStr.append(ch);
+		}
+		return randStr.toString();
+
+	}
+
+	/**
+	 * This method generates random numbers
+	 * 
+	 * @return int
+	 */
+	private int getRandomNumber() {
+		int randomInt = 0;
+		Random randomGenerator = new Random();
+		randomInt = randomGenerator.nextInt(CHAR_LIST.length());
+		if (randomInt - 1 == -1) {
+			return randomInt;
+		} else {
+			return randomInt - 1;
+		}
 	}
 
 }
